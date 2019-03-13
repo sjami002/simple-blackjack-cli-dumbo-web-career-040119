7c7ba2ca88a7f4771d136a3e6b8dff189ec66e2e
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 20:57:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -70,7 +70,7 @@ def runner
  user_hand = initial_round
  card_total = user_hand
 
- until card_total >= 21
+ until card_total > 21
    card_total = hit?(card_total)
    display_card_total(card_total)
  end
