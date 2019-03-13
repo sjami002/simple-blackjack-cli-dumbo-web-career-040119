38f6edeeaa4eac04af64767ea859d01e74e68827
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 20:59:24 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,63 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  total = deal_card + deal_card
+  puts "Your cards add up to #{total}"
+  total
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  user_input = get_user_input
+
+  if user_input != 'h' && user_input != 's'
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+
+  if user_input == 'h'
+    new_card = deal_card
+    user_hand += new_card
+  elsif user_input == 's'
+    user_hand
+  end
+  user_hand
 end
 
 def invalid_command
   # code invalid_command here
+  puts "invalid"
 end
 
 #####################################################
@@ -40,5 +66,13 @@ end
 
 def runner
   # code runner here
+  welcome
+ user_hand = initial_round
+ card_total = user_hand
+
+ until card_total > 21
+   card_total = hit?(card_total)
+   display_card_total(card_total)
+ end
+ end_game(card_total)
 end
-    
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/blackjack.rb"
-
+require_relative "../lib/runner.rb"
 RSpec.configure do |config|
 # config here
 end
